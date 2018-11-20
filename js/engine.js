5948153766de9c16b179c0893d7a41f8b3a56a0b
Aminat Amoo <aminaamoo@gmail.com>
2018-11-20 10:27:22 +0000

    replaced if statements with switch statement and combined 3 button selectors into 1

--- js/engine.js
+++ js/engine.js
@@ -1,79 +1,58 @@
 
 /*Variables needed*/
 
-var choices = ["Rock", "Paper", "Scissors"];
+var choices = ["rock", "paper", "scissors"];
 var played = 0;
 var won = 0;
 var lost = 0;
 var draw = 0;
 
 
-
-
-
 /*Listen for button clicks*/
 
-$("#rock").click(function(event){
-  var play_you = "Rock";
+$("button").click(function(event){
+  var play_you = this.getAttribute("id");
   $("#play_you").text(play_you);
   var play_computer = computer_move();
   compare(play_you, play_computer);
 })
 
-$("#paper").click(function(event){
-  var play_you = "Paper";
-  $("#play_you").text(play_you);
-  var play_computer = computer_move();
-  compare(play_you, play_computer);
-})
-
-$("#scissors").click(function(event){
-  var play_you = "Scissors";
-  $("#play_you").text(play_you);
-  var play_computer = computer_move();
-  compare(play_you, play_computer);
-})
 
 function computer_move(){
   var play_computer = Math.floor(Math.random()*(3 - 0)) + 0;
   play_computer = choices[play_computer];
   $("#play_computer").text(play_computer);
-
-  /*$("#game_won").empty();
-  $("#game_lost").empty(verdict);
-  $("#game_draw").empty(verdict);*/
-
   return play_computer;
 }
 
 function compare(you, computer){
 
   var verdict = 'lose';
+
   if (you == computer){
-    verdict = 'draw';
+
+        verdict = 'draw';
   }
-  if ((you == "Rock") && (computer == "Scissors")){
-    verdict = 'win';
-  }
-  if ((you == "Paper") && (computer == "Rock")){
-    verdict = 'win';
-  }
-  if ((you == "Scissors") && (computer == "Paper")){
-    verdict = 'win';
-  }
-  if (verdict == 'win'){
-    won++;
-    $("#game_won").text(won);
+  if (((you == "rock") && (computer == "scissors"))||
+     ((you == "paper") && (computer == "rock"))||
+     ((you == "scissors") && (computer == "paper"))){
+
+        verdict = 'win';
   }
 
-  if (verdict == 'lose'){
-    lost++;
-    $("#game_lost").text(lost);
-  }
-
-  if (verdict == 'draw'){
-    draw++;
-    $("#game_draw").text(draw);
+  switch (verdict){
+    case 'win':
+      won++;
+      $("#game_won").text(won);
+      break;
+    case 'lose':
+      lost++;
+      $("#game_lost").text(lost);
+      break;
+    case 'draw':
+      draw++;
+      $("#game_draw").text(draw);
+      break;
   }
   $("#verdict").text(verdict);
 
@@ -81,5 +60,4 @@ function compare(you, computer){
   $("#game_played").text(played);
 
   console.log(verdict);
-
 }
