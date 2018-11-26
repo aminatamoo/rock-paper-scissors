c9706ed8bccf62895083953e4bcab500458f496a
Aminat Amoo <aminaamoo@gmail.com>
2018-11-26 12:36:54 +0000

    first commit

--- js/engine.js
+++ js/engine.js
@@ -6,17 +6,21 @@ var played = 0;
 var won = 0;
 var lost = 0;
 var draw = 0;
+/*var remainder = played % 5;*/
 
 
-/*Listen for button clicks*/
+//Listen for button clicks
 
-$("button").click(function(event){
+$("img").click(function(event){
   var play_you = this.getAttribute("id");
   $("#play_you").text(play_you);
   var play_computer = computer_move();
   compare(play_you, play_computer);
+  assign_winner();
+  close();
 })
 
+//Functions
 
 function computer_move(){
   var play_computer = Math.floor(Math.random()*(3 - 0)) + 0;
@@ -59,5 +63,26 @@ function compare(you, computer){
   played++;
   $("#game_played").text(played);
 
-  console.log(verdict);
 }
+
+//Modal functionality
+
+
+function assign_winner(){
+
+if ((played == 20) && (won > lost )){
+  $('#winner').show();
+  $('#loser').hide();
+}
+else if ((played == 20) && (won < lost )){
+  $('#loser').show();
+  $('#winner').hide();
+}
+}
+
+/*function close(){
+  $('.main_close').click(function (event){
+    $('#loser').hide();
+    $('#winner').hide();
+});
+}*/
